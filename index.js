6de8d56f9cbeb7dc6b92e590c751c2dd62452b68
dange-marie <angelodsu22@gmail.com>
2025-05-19 10:55:18 +0100

    feat: add new note command

--- index.js
+++ index.js
@@ -2,5 +2,39 @@
 
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
+import { addNote, findNotes, removeAllNotes, removeNote } from './src/note.js'
 
-console.log('start')
+const printNote = (notes) => {
+    console.log("Note: ", notes.content, "\t", notes.tags.join(', '))
+    console.log('id: ', notes.id)
+    console.log('\n')
+}
+
+yargs(hideBin(process.argv))
+    .command(
+        'new <note>',
+        'Create new Note',
+        yargs => {
+            return yargs.positional(
+                'note',
+                {
+                    description: 'The content of the note you want to create',
+                    type: 'string'
+                }
+            )
+        },
+        async (argv) => {
+            const tags = argv.tags ? argv.tags.split(' ') : []
+            const note = argv.note
+            const {id} = await addNote(note, tags)
+            console.log('Note added!', id)
+        }
+    )
+    .option('tags', {
+        alias: 't',
+        type: 'string',
+        description: 'tags to add to the note'
+    })
+    .demandCommand(1)
+    .parse()
+
